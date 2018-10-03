04f7d95fa509dab3bf4b6428f5fb0ff2d180778f
Laurens D'Hondt <laurens.dhondt@MacBook-Pro-59.local>
2018-10-03 12:53:37 +0200

    remove console

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <div id="console" style="position:absolute;z-index:9999lleft:0;bottom:0;width:100vw;height:200px;padding:15px;background-color:#ffffff;border-top:solid 1px #000000;"></div>
-
     <div
       class="app-container padding-30 text-center d-flex align-items-center"
       v-bg-src="{ id: background.id, appLink: background.appLink}"
